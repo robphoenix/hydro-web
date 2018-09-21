50ab03b9055caa6ba847d632f218c23ed762e12d
Rob Phoenix <rob.phoenix@bet365.com>
2018-09-21 10:40:18 +0100

    Add /monitors route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MonitorsComponent } from './monitors/monitors.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/monitors', pathMatch: 'full' },
+  { path: 'monitors', component: MonitorsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
