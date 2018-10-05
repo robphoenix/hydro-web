2ae04a302faef3cd377cd10522b3f632bd314b86
constancasimas <constanca.simas@bet365.com>
2018-10-05 09:29:17 +0100

    Adding handling for OPTIONS preflights.

--- server/main.go
+++ server/main.go
@@ -20,6 +20,16 @@ func main() {
 
   // Router for JSON Mock API
   jsonRouter := mux.NewRouter()
+
+  // Dealing with CORS and preflight requests
+  jsonRouter.Methods("OPTIONS").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Access-Control-Allow-Origin", "*")
+    w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+    w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, Access-Control-Request-Headers, Access-Control-Request-Method, Connection, Host, Origin, User-Agent, Referer, Cache-Control, X-header")
+    w.WriteHeader(http.StatusNoContent)
+    return
+  })
+
   jsonRouter.HandleFunc("/liveMonitors", liveMonitors).Methods("GET")
   jsonRouter.HandleFunc("/liveMonitors/{id}", liveMonitorsByID).Methods("GET")
   jsonRouter.HandleFunc("/monitorData/{id}", monitorDataByID).Methods("GET")
