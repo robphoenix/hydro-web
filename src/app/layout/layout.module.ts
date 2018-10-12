4812bae854ffcd038ddda088c8ce714684406861
Rob Phoenix <rob.phoenix@bet365.com>
2018-10-12 11:42:56 +0100

    Fix broken routerLink's

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  imports: [CommonModule, MatToolbarModule, MatButtonModule],
+  imports: [CommonModule, RouterModule, MatToolbarModule, MatButtonModule],
   exports: [HeaderComponent],
   declarations: [HeaderComponent],
 })
