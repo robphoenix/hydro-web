204d1c5d89d9e4eeb605b764a59209c4ab8597ff
Rob Phoenix <rob.phoenix@bet365.com>
2018-10-19 09:56:46 +0100

    Remove unnecessary abstraction

--- src/app/user/auth.service.spec.ts
+++ src/app/user/auth.service.spec.ts
@@ -4,6 +4,7 @@ import { AuthService } from './auth.service';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { JwtHelperService } from '@auth0/angular-jwt';
+import { IAccessToken } from './access-token';
 
 describe('AuthService', () => {
   let mockJwtHelperService;
@@ -25,28 +26,5 @@ describe('AuthService', () => {
   it('should be created', () => {
     const service: AuthService = TestBed.get(AuthService);
     expect(service).toBeTruthy();
-  });
-
-  describe('isValidToken', () => {
-    it('should return opposite to JWTHelperService.isTokenExpired()', () => {
-      mockJwtHelperService.isTokenExpired.and.returnValue(true);
-      const service: AuthService = TestBed.get(AuthService);
-      expect(service.isValidToken()).toEqual(false);
-    });
-
-    it('should be called with given token', () => {
-      const service: AuthService = TestBed.get(AuthService);
-      const token = 'token';
-      service.isValidToken(token);
-      expect(mockJwtHelperService.isTokenExpired).toHaveBeenCalledWith(token);
-    });
-
-    it('should be called with access token if no token given', () => {
-      const service: AuthService = TestBed.get(AuthService);
-      const token = 'access_token';
-      service.accessToken = token;
-      service.isValidToken();
-      expect(mockJwtHelperService.isTokenExpired).toHaveBeenCalledWith(token);
-    });
   });
 });
