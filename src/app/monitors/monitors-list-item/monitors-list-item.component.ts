ecf2d78726384725e5aedafa9bf908b054b05a92
Rob Phoenix <rob.phoenix@bet365.com>
2018-11-22 10:17:42 +0000

    Add Delete Monitor dialog

--- src/app/monitors/monitors-list-item/monitors-list-item.component.ts
+++ src/app/monitors/monitors-list-item/monitors-list-item.component.ts
@@ -1,5 +1,8 @@
 import { Component, Input } from '@angular/core';
 import { IMonitor, IAction } from '../monitor';
+import { MatDialog } from '@angular/material';
+import { MonitorDeleteDialogComponent } from '../monitor-delete-dialog/monitor-delete-dialog.component';
+import { IDeleteDialogData } from '../delete-dialog-data';
 
 @Component({
   selector: 'app-monitors-list-item',
@@ -24,6 +27,8 @@ export class MonitorsListItemComponent {
     'Network Security': 'net-sec',
   };
 
+  constructor(public dialog: MatDialog) {}
+
   /**
    * Returns a single string that contains the list of actions.
    *
@@ -33,5 +38,15 @@ export class MonitorsListItemComponent {
    */
   actionNames(actions: IAction[]): string {
     return actions.map((action) => action.name).join('\n');
+  }
+
+  openDeleteDialog() {
+    const dialogRef = this.dialog.open(MonitorDeleteDialogComponent, {
+      data: { topic: this.monitor.topic } as IDeleteDialogData,
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log({ result });
+    });
   }
 }
