914629631701616d203f11772898b56ed8de2fd8
Rob Phoenix <rob.phoenix@bet365.com>
2018-12-06 09:29:09 +0000

    Delete sql pipe

--- src/app/shared/format-sql.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FormatSqlPipe } from './format-sql.pipe';
-
-describe('FormatSqlPipe', () => {
-  it('create an instance', () => {
-    const pipe = new FormatSqlPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
