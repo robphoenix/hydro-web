d5d7178dfbca775fc8f5d1d0cd3bd0f16482c1c7
Rob Phoenix <rob.phoenix@bet365.com>
2018-12-12 13:04:36 +0000

    Preload lazy loaded routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,25 @@
 import { AuthGuard } from './user/auth.guard';
 import { LoginComponent } from './user/login/login.component';
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { RouterModule, PreloadAllModules } from '@angular/router';
 import { LoginGuard } from './user/login.guard';
 import { UserModule } from './user/user.module';
 
 @NgModule({
   imports: [
     UserModule,
-    RouterModule.forRoot([
-      {
-        path: 'monitors',
-        canActivate: [AuthGuard],
-        loadChildren: './monitors/monitors.module#MonitorsModule',
-      },
-      { path: 'login', component: LoginComponent, canActivate: [LoginGuard] },
-      { path: '', redirectTo: 'monitors', pathMatch: 'full' },
-    ]),
+    RouterModule.forRoot(
+      [
+        {
+          path: 'monitors',
+          canActivate: [AuthGuard],
+          loadChildren: './monitors/monitors.module#MonitorsModule',
+        },
+        { path: 'login', component: LoginComponent, canActivate: [LoginGuard] },
+        { path: '', redirectTo: 'monitors', pathMatch: 'full' },
+      ],
+      { preloadingStrategy: PreloadAllModules },
+    ),
   ],
   exports: [RouterModule],
 })
