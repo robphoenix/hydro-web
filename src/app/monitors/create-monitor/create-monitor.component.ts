cacc415a9e7f0bed03f310a15dd7688cf5afd23f
Rob Phoenix <rob.phoenix@bet365.com>
2018-12-17 17:19:04 +0000

    Add categories

--- src/app/monitors/create-monitor/create-monitor.component.ts
+++ src/app/monitors/create-monitor/create-monitor.component.ts
@@ -1,12 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import {
   FormGroup,
   FormBuilder,
   Validators,
-  AbstractControl,
   FormControl,
 } from '@angular/forms';
-import { debounceTime } from 'rxjs/operators';
+import { debounceTime, startWith, map } from 'rxjs/operators';
+import {
+  MatAutocomplete,
+  MatChipInputEvent,
+  MatAutocompleteSelectedEvent,
+} from '@angular/material';
+import { ENTER, COMMA, SPACE } from '@angular/cdk/keycodes';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-create-monitor',
@@ -19,6 +25,50 @@ export class CreateMonitorComponent implements OnInit {
   formGroup: FormGroup;
   nameControl: FormControl;
   descriptionControl: FormControl;
+  categoriesControl: FormControl;
+
+  @ViewChild('categoryInput')
+  categoryInput: ElementRef<HTMLInputElement>;
+  @ViewChild('auto')
+  matAutocomplete: MatAutocomplete;
+
+  categoriesOptions = {
+    selectable: true,
+    removable: true,
+    addOnBlur: true,
+    separatorKeysCodes: [ENTER, COMMA, SPACE],
+  };
+  selectedCategories: string[] = [];
+  filteredCategories: Observable<string[]>;
+  availableCategories: string[] = [
+    'script-attack',
+    'Mobile',
+    'Sports Book',
+    'JohnSnow',
+    'BetSlip',
+    'GavinEdwards',
+    'Scrapers',
+    'China Arbs',
+    'bettingslip',
+    'Extra',
+    'FRM',
+    'Alerts',
+    'NewLoginDefault',
+    'Members',
+    'Monitor',
+    'Blocking',
+    'Investigation',
+    'HoneyPot',
+    'Publisher',
+    'Datacenter',
+    'LoginAttack',
+    'Bookmaker',
+    'hostingfacility',
+    'geo',
+    'Martin',
+    'OpenAccount',
+    'Ragbag',
+  ];
 
   validationMessages = {
     name: {
@@ -36,10 +86,12 @@ export class CreateMonitorComponent implements OnInit {
       Validators.pattern('[a-zA-Z0-9]+'),
     ]);
     this.descriptionControl = new FormControl('', [Validators.required]);
+    this.categoriesControl = new FormControl(this.selectedCategories);
     this.formGroup = this.fb.group({
       name: this.nameControl,
       status: ['offline', Validators.required],
       description: this.descriptionControl,
+      categories: this.categoriesControl,
     });
   }
 
@@ -55,5 +107,50 @@ export class CreateMonitorComponent implements OnInit {
         this.descriptionControl.markAsDirty();
         this.descriptionControl.markAsTouched();
       });
+
+    this.filteredCategories = this.categoriesControl.valueChanges.pipe(
+      startWith(null),
+      map((term: string) => {
+        if (!term) {
+          return this.availableCategories.slice();
+        }
+        return this.availableCategories.filter((c) =>
+          c.toLowerCase().includes(term.toLowerCase()),
+        );
+      }),
+    );
+  }
+
+  add(event: MatChipInputEvent): void {
+    const input = event.input;
+    const value = (event.value || '').trim();
+
+    if (value && !this.selectedCategories.includes(value)) {
+      this.selectedCategories.push(value);
+    }
+
+    if (input) {
+      input.value = '';
+    }
+
+    this.categoriesControl.setValue(null);
+  }
+
+  remove(category: string): void {
+    const index = this.selectedCategories.indexOf(category);
+
+    if (index >= 0) {
+      this.selectedCategories.splice(index, 1);
+      this.availableCategories.push(category);
+    }
+  }
+
+  selected(event: MatAutocompleteSelectedEvent): void {
+    const value = event.option.viewValue;
+    const index = this.availableCategories.indexOf(value);
+    this.selectedCategories.push(value);
+    this.availableCategories.splice(index, 1);
+    this.categoryInput.nativeElement.value = '';
+    this.categoriesControl.setValue(null);
   }
 }
