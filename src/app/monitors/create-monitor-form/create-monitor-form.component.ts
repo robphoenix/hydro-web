6664e327ab5e7de90cb343bc1309517d038a4c4e
Rob Phoenix <rob.phoenix@bet365.com>
2018-12-20 11:33:19 +0000

    Add codemirror

--- src/app/monitors/create-monitor-form/create-monitor-form.component.ts
+++ src/app/monitors/create-monitor-form/create-monitor-form.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ElementRef,
+  AfterViewInit,
+  ViewChild,
+} from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import {
   MatChipInputEvent,
@@ -10,12 +16,26 @@ import { ICategory } from '../monitor';
 import { MonitorsService } from '../monitors.service';
 import { debounceTime, startWith, map } from 'rxjs/operators';
 
+import { Editor, EditorFromTextArea } from 'codemirror';
+import 'codemirror/mode/sql/sql';
+declare var require: any;
+
 @Component({
   selector: 'app-create-monitor-form',
   templateUrl: './create-monitor-form.component.html',
   styleUrls: ['./create-monitor-form.component.scss'],
 })
-export class CreateMonitorFormComponent implements OnInit {
+export class CreateMonitorFormComponent implements OnInit, AfterViewInit {
+  codemirrorConfig = {
+    mode: 'text/x-sql',
+    lineWrapping: false,
+    lineNumbers: true,
+    theme: 'base16-light',
+  };
+  codeMirror: EditorFromTextArea;
+  @ViewChild('ref')
+  ref: ElementRef;
+
   formGroup: FormGroup;
 
   categoriesAutocompleteOptions = {
@@ -53,6 +73,20 @@ export class CreateMonitorFormComponent implements OnInit {
       description: ['', Validators.required],
       categories: [this.selectedCategories],
     });
+  }
+
+  ngAfterViewInit() {
+    const { fromTextArea } = require('codemirror');
+    this.codeMirror = fromTextArea(
+      this.ref.nativeElement,
+      this.codemirrorConfig,
+    );
+
+    this.codeMirror.on('change', (editor: Editor) => {
+      const value = editor.getDoc().getValue();
+      console.log({ value });
+    });
+    this.codeMirror.setSize('600px', '600px');
   }
 
   ngOnInit() {
