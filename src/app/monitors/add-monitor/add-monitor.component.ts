7e3bef53faa118af433116ee9500933c63d50340
Rob Phoenix <rob.phoenix@bet365.com>
2019-01-03 16:51:53 +0000

    Formatting

--- src/app/monitors/add-monitor/add-monitor.component.ts
+++ src/app/monitors/add-monitor/add-monitor.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-add-monitor',
   templateUrl: './add-monitor.component.html',
-  styleUrls: ['./add-monitor.component.scss']
+  styleUrls: ['./add-monitor.component.scss'],
 })
 export class AddMonitorComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
