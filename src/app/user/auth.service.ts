27cedbe15f7f7771ded3be2083421ccee592fec2
Rob Phoenix <rob.phoenix@bet365.com>
2019-01-16 12:56:17 +0000

    Update console-api URL

--- src/app/user/auth.service.ts
+++ src/app/user/auth.service.ts
@@ -50,7 +50,7 @@ const httpOptions = {
   providedIn: 'root',
 })
 export class AuthService {
-  readonly baseUrl = `http://mn2splmfe001sd0:6080`;
+  readonly baseUrl = `http://mn2formlt0001d0:6080`;
   readonly loginUrl = `${this.baseUrl}/login`;
   readonly refreshUrl = `${this.baseUrl}/p/refresh`;
 
