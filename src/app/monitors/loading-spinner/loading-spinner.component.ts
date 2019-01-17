9015a50735b71dfc2b5de53dd529a980a4b21679
Rob Phoenix <rob.phoenix@bet365.com>
2019-01-17 14:53:14 +0000

    Update loading spinner

--- src/app/monitors/loading-spinner/loading-spinner.component.ts
+++ src/app/monitors/loading-spinner/loading-spinner.component.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-loading-spinner',
   templateUrl: './loading-spinner.component.html',
   styleUrls: ['./loading-spinner.component.scss'],
 })
-export class LoadingSpinnerComponent {}
+export class LoadingSpinnerComponent {
+  @Input()
+  title: string;
+}
