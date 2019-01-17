34db66254d91b966ecdddb3898070680f1a189db
Rob Phoenix <rob.phoenix@bet365.com>
2019-01-17 12:42:03 +0000

    Add monitor data types

--- src/app/monitors/monitor-data.ts
+++ src/app/monitors/monitor-data.ts
@@ -1,25 +1,13 @@
-/**
- * Describes the monitor data.
- *
- * @export
- * @interface MonitorData
- */
 export interface IMonitorData {
-  id: string;
-  esperItems: Array<IEsperItem[]>;
-  headers: string[];
-  timeStamp: Date;
+  address: string;
+  body: { messages: IMonitorDataMessage[] };
 }
 
-/**
- * Describes the monitor's esper items,
- * this is the main set of variable data
- * that will be displayed.
- *
- * @export
- * @interface EsperItem
- */
-export interface IEsperItem {
-  key: string;
-  value: string;
+export interface IMonitorDataMessage {
+  feedName: string;
+  attributes: IMonitorDataAttributes;
+}
+
+export interface IMonitorDataAttributes {
+  [key: string]: any;
 }
