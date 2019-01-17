d3d5c453a987fad0f0e15e2b3f848a281aa99cb8
Rob Phoenix <rob.phoenix@bet365.com>
2019-01-17 14:53:26 +0000

    Display data in table

--- src/app/monitors/monitor/monitor.component.ts
+++ src/app/monitors/monitor/monitor.component.ts
@@ -1,9 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, OnChanges } from '@angular/core';
 import { Location } from '@angular/common';
 import { IMonitor } from '../monitor';
 import { MonitorsService } from '../monitors.service';
 import { ActivatedRoute } from '@angular/router';
 import * as EventBus from 'vertx3-eventbus-client';
+import { MatTableDataSource, MatPaginator } from '@angular/material';
+import {
+  IMonitorDataAttributes,
+  IMonitorData,
+  IMonitorDataMessage,
+} from '../monitor-data';
 
 /**
  * Describes a single monitor.
@@ -17,24 +23,38 @@ import * as EventBus from 'vertx3-eventbus-client';
   templateUrl: './monitor.component.html',
   styleUrls: ['./monitor.component.scss'],
 })
-export class MonitorComponent implements OnInit {
-  monitor: IMonitor;
+export class MonitorComponent implements OnInit, OnChanges {
+  private eventBusUrl = 'http://mn2formlt0002d0:6081/eventbus';
+  private eventBusAddress = 'result.pub.output';
   private eb: EventBus.EventBus;
-  private headers: any;
-  public messages: string[] = [];
+  private headers: { [key: string]: any } = {};
+
+  monitor: IMonitor;
+  dataSource: MatTableDataSource<IMonitorDataAttributes>;
+  displayedColumns: string[];
+  attributes: IMonitorDataAttributes[] = [];
+
+  @ViewChild(MatPaginator)
+  private paginator: MatPaginator;
 
   constructor(
     private route: ActivatedRoute,
     private location: Location,
     private monitorService: MonitorsService,
   ) {
-    this.eb = new EventBus('http://mn2formlt0002d0:6081/eventbus');
-
-    this.headers = {};
+    this.eb = new EventBus(this.eventBusUrl);
   }
 
   ngOnInit() {
+    this.dataSource = new MatTableDataSource(this.attributes);
+    this.dataSource.paginator = this.paginator;
     this.getMonitor();
+  }
+
+  ngOnChanges(): void {
+    if (this.dataSource) {
+      this.dataSource.data = this.attributes;
+    }
   }
 
   /**
@@ -55,14 +75,25 @@ export class MonitorComponent implements OnInit {
   subscribe() {
     this.eb.onopen = () => {
       this.eb.registerHandler(
-        'result.pub.output',
+        this.eventBusAddress,
         this.headers,
-        (error, message) => {
+        (error, data: IMonitorData) => {
           if (error) {
-            console.log(error);
+            console.error(error);
           }
-          this.messages.push(message);
-          console.log(message);
+
+          const messages: IMonitorDataMessage[] = data.body.messages;
+
+          if (!this.displayedColumns) {
+            this.displayedColumns = Object.keys(messages[0].attributes);
+          }
+
+          this.attributes = messages.map(
+            (message: IMonitorDataMessage) => message.attributes,
+          );
+          this.dataSource.data = this.attributes;
+
+          console.log(data);
         },
       );
     };
