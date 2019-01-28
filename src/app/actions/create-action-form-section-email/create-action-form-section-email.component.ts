e538173df33ab7839e62fa4819896e6a578c90e2
Rob Phoenix <rob.phoenix@bet365.com>
2019-01-28 16:00:31 +0000

    Add Email action form component

--- src/app/actions/create-action-form-section-email/create-action-form-section-email.component.ts
+++ src/app/actions/create-action-form-section-email/create-action-form-section-email.component.ts
@@ -6,6 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-action-form-section-email.component.scss'],
 })
 export class CreateActionFormSectionEmailComponent implements OnInit {
+  emails: string[] = ['Forensic Monitoring', 'FRM', 'OTS'];
+  selectedEmails: string[];
+  templates: string[] = ['Template 1', 'Template 2', 'Template 3'];
+  selectedTemplate: string;
+  emailTypes: string[] = ['Rate', 'Batch', 'Alert'];
+  selectedEmailType: string;
+  fields: string[] = ['sip', 'stk'];
+  selectedFields: string[];
+  sendLimit: number;
+  batchTime: string;
+  batchTimeOfDay: string;
+
   constructor() {}
 
   ngOnInit() {}
