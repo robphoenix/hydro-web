f8e8f238cb36bcf4296bffbd69d1756ac77c26ae
Rob Phoenix <rob.phoenix@bet365.com>
2019-02-12 14:21:49 +0000

    Include Duration and Delay in Block form

--- src/app/actions/create-action-form-section-block/create-action-form-section-block.component.ts
+++ src/app/actions/create-action-form-section-block/create-action-form-section-block.component.ts
@@ -1,11 +1,19 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter, Input, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+
+interface IBlockValue {
+  formGroup: FormGroup;
+  label: string;
+  units: string[];
+}
 
 @Component({
   selector: 'app-create-action-form-section-block',
   templateUrl: './create-action-form-section-block.component.html',
   styleUrls: ['./create-action-form-section-block.component.scss'],
 })
-export class CreateActionFormSectionBlockComponent {
+export class CreateActionFormSectionBlockComponent implements OnInit {
+  // temporary
   blockItems: string[] = [
     'IP Address',
     'IP Range',
@@ -14,41 +22,101 @@ export class CreateActionFormSectionBlockComponent {
     'UQID',
     'STK',
   ];
-  selected = '';
-  durationLength: number;
-  durationType: string;
-  delayLength: number;
-  delayType: string;
-  permanently = false;
 
   name: string;
+  durationFormGroup: FormGroup;
+  delayFormGroup: FormGroup;
+  blockValues: IBlockValue[];
+  unitsInSeconds: { [key: string]: number } = {
+    seconds: 1,
+    minutes: 60,
+    hours: 60 * 60,
+    days: 60 * 60 * 24,
+  };
+
+  @Input()
+  parent: FormGroup;
 
   @Output()
   nameChange = new EventEmitter<string>();
 
-  constructor() {
-    this.updateName();
+  @Output()
+  durationChange = new EventEmitter<number>();
+
+  @Output()
+  delayChange = new EventEmitter<number>();
+
+  constructor(private fb: FormBuilder) {
+    this.durationFormGroup = this.fb.group({
+      value: [''],
+      unit: [''],
+    });
+    this.delayFormGroup = this.fb.group({
+      value: [''],
+      unit: [''],
+    });
+    this.blockValues = [
+      {
+        formGroup: this.durationFormGroup,
+        label: 'Block Duration',
+        units: ['minutes', 'hours', 'days'],
+      },
+      {
+        formGroup: this.delayFormGroup,
+        label: 'Block Delay',
+        units: ['seconds', 'minutes', 'hours'],
+      },
+    ];
   }
 
-  updateName() {
-    let duration = '';
-    if (!this.permanently && this.durationLength && this.durationType) {
-      duration = `for ${this.durationLength} ${this.durationType}`;
+  onBlockValueChange() {
+    const durationUnit = this.durationFormGroup.get('unit').value;
+    const durationValue = this.durationFormGroup.get('value').value;
+
+    if (durationValue && durationUnit) {
+      const numberOfSeconds = this.unitsInSeconds[durationUnit];
+      const duration = durationValue * numberOfSeconds;
+      this.durationChange.emit(duration);
+      this.onNameChange();
     }
 
-    let delay = '';
+    const delayUnit = this.delayFormGroup.get('unit').value;
+    const delayValue = this.delayFormGroup.get('value').value;
 
-    if (!this.permanently && this.delayLength && this.delayType) {
-      delay = `with up to ${this.delayLength} ${this.delayType} random delay`;
+    if (delayValue && delayUnit) {
+      const numberOfSeconds = this.unitsInSeconds[delayUnit];
+      const delay = delayValue * numberOfSeconds;
+      this.delayChange.emit(delay);
+      this.onNameChange();
     }
+  }
 
-    this.name = `Block ${this.selected} ${duration} ${delay} ${
-      this.permanently ? 'permanently' : ''
+  ngOnInit(): void {
+    this.onNameChange();
+  }
+
+  onNameChange() {
+    const permanently = this.parent.get('permanently').value;
+    const itemToBlockOn = this.parent.get('item').value.trim();
+    const durationValue = this.durationFormGroup.get('value').value.trim();
+    const durationUnit = this.durationFormGroup.get('unit').value.trim();
+    const delayValue = this.delayFormGroup.get('value').value.trim();
+    const delayUnit = this.delayFormGroup.get('unit').value.trim();
+
+    const duration =
+      !permanently && durationValue && durationUnit
+        ? `for ${durationValue} ${durationUnit}`
+        : '';
+
+    const delay =
+      !permanently && delayValue && delayUnit
+        ? `with up to ${delayValue} ${delayUnit} random delay`
+        : '';
+
+    this.name = `Block ${itemToBlockOn} ${duration} ${delay} ${
+      permanently ? 'permanently' : ''
     }`;
-    this.nameChange.emit(this.name);
-  }
 
-  blockPermanently() {
-    this.permanently = true;
+    this.nameChange.emit(this.name);
   }
 }
--- src/app/actions/create-action-form/create-action-form.component.ts
+++ src/app/actions/create-action-form/create-action-form.component.ts
@@ -8,18 +8,38 @@ import { FormGroup, FormBuilder } from '@angular/forms';
 })
 export class CreateActionFormComponent implements OnInit {
   createActionForm: FormGroup;
+  blockDataForm: FormGroup;
 
   @Input()
   title: string;
 
   constructor(private fb: FormBuilder) {
+    this.blockDataForm = this.fb.group({
+      item: [''],
+      permanently: [false],
+      duration: [0],
+      delay: [0],
+    });
     this.createActionForm = this.fb.group({
       name: [''],
+      description: [''],
+      type: ['block'],
+      blockData: this.blockDataForm,
     });
   }
 
-  updateName(name: string) {
-    this.createActionForm.get('name').setValue(name);
+  onNameChange(name: string) {
+    this.createActionForm.get('name').setValue(name.trim());
+    const form = this.createActionForm.value;
+    console.log({ form });
+  }
+
+  onDurationChange(duration: number) {
+    this.blockDataForm.get('duration').setValue(duration);
+  }
+
+  onDelayChange(delay: number) {
+    this.blockDataForm.get('delay').setValue(delay);
   }
 
   ngOnInit() {}
