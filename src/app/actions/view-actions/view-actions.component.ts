4e1098d58118ec95bda19949cca367d7d86f623a
Rob Phoenix <rob.phoenix@bet365.com>
2019-04-09 17:09:01 +0100

    Add group filter

--- src/app/actions/view-actions/view-actions.component.ts
+++ src/app/actions/view-actions/view-actions.component.ts
@@ -11,6 +11,17 @@ export class ViewActionsComponent implements OnInit {
   public actions: IActions[] = [];
   public filteredActions: IActions[] = [];
   public searchTerm: string;
+  public group = 'all';
+  public icons: { [type: string]: { icon: string; colour: string } } = {
+    actions: {
+      icon: 'play_circle_filled',
+      colour: 'orange',
+    },
+    block: { icon: 'block', colour: 'red' },
+    email: { icon: 'mail_outline', colour: 'green' },
+    store: { icon: 'check', colour: 'blue' },
+    other: { icon: 'subject', colour: 'yellow' },
+  };
 
   constructor(private actionsService: ActionsService) {}
 
@@ -27,16 +38,22 @@ export class ViewActionsComponent implements OnInit {
     });
   }
 
-  filterActions() {
+  filterActions(group?: string) {
+    this.group = group || this.group;
+    console.log(this.searchTerm);
+    console.log(this.group);
     this.filteredActions = this.actions.filter((action: IActions) => {
-      const regex: RegExp = new RegExp(
-        this.searchTerm.trim().toLowerCase(),
-        'gi',
-      );
+      const searchTerm: string = this.searchTerm || '';
+
+      const regex: RegExp = new RegExp(searchTerm.trim().toLowerCase(), 'gi');
       const match = `${action.name.toLowerCase()} ${action.description.toLowerCase()}`.match(
         regex,
       );
-      return match && match.length > 0;
+      const matchesSearchTerm: boolean = match && match.length > 0;
+      const isInGroup: boolean =
+        this.group === 'all' ? true : action.group === this.group;
+
+      return matchesSearchTerm && isInGroup;
     });
   }
 }
