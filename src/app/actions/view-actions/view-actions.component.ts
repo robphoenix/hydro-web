61a1d7d7bab3c4f8e928f502b1524049ab393d8a
Rob Phoenix <rob.phoenix@bet365.com>
2019-04-09 11:11:15 +0100

    Change view filters to use a list

--- src/app/actions/view-actions/view-actions.component.ts
+++ src/app/actions/view-actions/view-actions.component.ts
@@ -1,12 +1,6 @@
-import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IActions } from '../actions';
 import { ActionsService } from '../actions.service';
-import {
-  MatTableDataSource,
-  MatSort,
-  Sort,
-  MatSortable,
-} from '@angular/material';
 
 @Component({
   selector: 'app-view-actions',
@@ -15,54 +9,34 @@ import {
 })
 export class ViewActionsComponent implements OnInit {
   public actions: IActions[] = [];
-
-  public dataSource: MatTableDataSource<IActions>;
-  public displayedColumns = ['name', 'group', 'metadata'];
-
-  @ViewChild(MatSort)
-  sort: MatSort;
+  public filteredActions: IActions[] = [];
+  public searchTerm: string;
 
   constructor(private actionsService: ActionsService) {}
-
-  getActions() {
-    this.actionsService.getActions().subscribe((actions: IActions[]) => {
-      this.actions = actions.sort((a: IActions, b: IActions) =>
-        this.compare(a.name.toLowerCase(), b.name.toLowerCase(), true),
-      );
-      this.dataSource = new MatTableDataSource(this.actions);
-      this.dataSource.sort = this.sort;
-    });
-  }
 
   ngOnInit() {
     this.getActions();
   }
 
-  sortData(sort: Sort) {
-    const data = this.actions.slice();
-    if (!sort.active || sort.direction === '') {
-      return;
-    }
-
-    this.dataSource.data = data.sort((a, b) => {
-      const isAsc = sort.direction === 'asc';
-      switch (sort.active) {
-        case 'name':
-          return this.compare(
-            a.name.toLowerCase(),
-            b.name.toLowerCase(),
-            isAsc,
-          );
-        case 'group':
-          return this.compare(a.group, b.group, isAsc);
-
-        default:
-          return 0;
-      }
+  getActions() {
+    this.actionsService.getActions().subscribe((actions: IActions[]) => {
+      this.actions = actions.sort((a: IActions, b: IActions) => {
+        return a.name.toLowerCase() < b.name.toLowerCase() ? -1 : 1;
+      });
+      this.filteredActions = this.actions;
     });
   }
 
-  compare(a: number | string, b: number | string, isAsc: boolean) {
-    return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
+  filterActions() {
+    this.filteredActions = this.actions.filter((action: IActions) => {
+      const regex: RegExp = new RegExp(
+        this.searchTerm.trim().toLowerCase(),
+        'gi',
+      );
+      const match = `${action.name.toLowerCase()} ${action.description.toLowerCase()}`.match(
+        regex,
+      );
+      return match && match.length > 0;
+    });
   }
 }
