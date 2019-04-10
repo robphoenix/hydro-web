6ac83f16ecb24305ff07cb3d4c5822279a7a3b99
Rob Phoenix <rob.phoenix@bet365.com>
2019-04-10 10:24:31 +0100

    Update action types

--- src/app/actions/actions.ts
+++ src/app/actions/actions.ts
@@ -1,20 +1,39 @@
 export interface IActions {
   id: number;
   name: string;
-  group: string;
+  group: ActionGroup;
   description: string;
   address: string;
   metadata: IActionsMetadataBlock | IActionsMetadataEmail;
   archived: boolean;
 }
 
+export enum ActionGroup {
+  Block = 'block',
+  Email = 'email',
+  Store = 'store',
+}
+
 export interface IActionsMetadataBlock {
-  type: string;
+  type: ActionsBlockType;
   blockTime: number;
-  blockTimeUnit: string;
+  blockTimeUnit: ActionsBlockTimeUnit;
   blockDelay: number;
-  blockDelayUnit: string;
+  blockDelayUnit: ActionsBlockDelayUnit;
   blockParameters: string[];
+  parameters: ActionParameters[];
+}
+
+export enum ActionsBlockTimeUnit {
+  Minutes = 'minutes',
+  Hours = 'hours',
+  Days = 'days',
+}
+
+export enum ActionsBlockDelayUnit {
+  Seconds = 'seconds',
+  Minutes = 'minutes',
+  Hours = 'hours',
 }
 
 export enum ActionsBlockType {
@@ -27,9 +46,10 @@ export interface IActionsMetadataEmail {
   emailSubject: string;
   emailAdresses: string;
   emailAlertFields: string;
+  parameters: ActionParameters[];
 }
 
-export enum BlockParameters {
+export enum ActionParameters {
   Sip = 'IP Address',
   IpRange = 'IP Range',
   Stk = 'Session token',
