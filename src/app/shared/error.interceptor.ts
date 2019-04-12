55063767b1758a8132e494833d8f849c2d3c7415
Rob Phoenix <rob.phoenix@bet365.com>
2019-04-12 12:36:06 +0100

    Update & Fix error interceptor

--- src/app/shared/error.interceptor.ts
+++ src/app/shared/error.interceptor.ts
@@ -28,40 +28,22 @@ export class ErrorInterceptor implements HttpInterceptor {
   ): Observable<HttpEvent<any>> {
     return next.handle(req).pipe(
       catchError((error: HttpErrorResponse) => {
+        console.error({ error });
+        if (!error.status) {
+          // we don't know what's happened, maybe the internet broke?
+          return throwError({
+            errorCode: 'UNKNOWN_ERROR',
+            message: 'Unknown error. Please check your network connection.',
+            cause: 'Unknown error. Please check your network connection.',
+          } as IErrorMessage);
+        }
+
         if (error.status === 401) {
           // auto logout if failed authentication response returned from api
           this.authService.logout();
         }
-        let msg: IErrorMessage;
 
-        if (error.status === 404) {
-          const { message } = error;
-          msg = {
-            errorCode: `${error.status}`,
-            message: message,
-            cause: error.statusText,
-          } as IErrorMessage;
-        } else if (error.error.length) {
-          // The backend returned an unsuccessful response code.
-          // Until otherwise necessary, we're only going to deal
-          // with the first error message sent back from the server
-          msg = error.error[0] as IErrorMessage;
-        } else if (!error.status) {
-          // we don't know what's happened, maybe the internet broke?
-          msg = {
-            errorCode: 'UNKNOWN_ERROR',
-            message: 'Unknown error. Please check your network connection.',
-            cause: 'Unknown error. Please check your network connection.',
-          } as IErrorMessage;
-        } else {
-          // A client-side or network error occurred
-          msg = {
-            errorCode: `${error.status}`,
-            message: error.message,
-            cause: error.statusText,
-          } as IErrorMessage;
-        }
-        return throwError(msg);
+        return throwError(error.error as IErrorMessage);
       }),
     );
   }
