52e5298aeb0131a3d086c5c5f4cc04e256932a50
Rob Phoenix <rob.phoenix@bet365.com>
2019-04-16 13:31:10 +0100

    Add info

--- src/app/actions/create-action-form-section-email/create-action-form-section-email.component.ts
+++ src/app/actions/create-action-form-section-email/create-action-form-section-email.component.ts
@@ -1,4 +1,4 @@
-import { Component, Output, EventEmitter, Input } from '@angular/core';
+import { Component, Output, EventEmitter, Input, OnInit } from '@angular/core';
 import { FormGroup, FormArray } from '@angular/forms';
 
 @Component({
@@ -6,10 +6,12 @@ import { FormGroup, FormArray } from '@angular/forms';
   templateUrl: './create-action-form-section-email.component.html',
   styleUrls: ['./create-action-form-section-email.component.scss'],
 })
-export class CreateActionFormSectionEmailComponent {
+export class CreateActionFormSectionEmailComponent implements OnInit {
   sendLimit: number;
   batchTime: string;
   batchTimeOfDay: string;
+
+  public selectedEmailType: string;
 
   public emailAddresses: string[];
 
@@ -33,6 +35,19 @@ export class CreateActionFormSectionEmailComponent {
 
   @Output()
   removeEmailAddress = new EventEmitter<number>();
+
+  emailTypeExplanation = {
+    // tslint:disable-next-line:max-line-length
+    Alert: `The properties you define below will be used to send out an email for every esper event. Once an alert value such as an IP (eg 1.1.1.1) or session token has been sent then it, and all other data in the same row, will be ignored for four days and any future emails will not reference it even though it was part of an esper event.`,
+    // tslint:disable-next-line:max-line-length
+    Batch: `The properties you define below will be used to batch esper events into email sent every day at the time specified`,
+    // tslint:disable-next-line:max-line-length
+    Rate: `The properties you define below will be used to send out emails at a maximum number per hour`,
+  };
+
+  ngOnInit(): void {
+    this.selectedEmailType = this.emailTypes[0];
+  }
 
   get emailAddressesArray(): FormArray {
     return this.parent.get('emailAddresses') as FormArray;
