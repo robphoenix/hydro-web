abe86bd8ebbc6ec9a08364b77592da1959887e21
Rob Phoenix <rob.phoenix@bet365.com>
2019-04-24 11:30:15 +0100

    Update login button

--- src/app/layout/navbar/navbar.component.ts
+++ src/app/layout/navbar/navbar.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { AuthService } from 'src/app/user/auth.service';
 
 @Component({
-  selector: 'app-navbar',
+  selector: 'hydro-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
 })
