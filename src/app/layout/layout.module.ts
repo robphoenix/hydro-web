339b4fa83e4da4c3d3944b1cce52494c1607983f
Rob Phoenix <rob@robphoenix.com>
2019-06-03 15:09:25 +0100

    Add footer with version

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -5,10 +5,11 @@ import { HydroMaterialModule } from '../material/material.module';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SharedModule } from '../shared/shared.module';
 import { LogotypeLinkComponent } from './logotype-link/logotype-link.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   imports: [CommonModule, SharedModule, RouterModule, HydroMaterialModule],
-  exports: [NavbarComponent],
-  declarations: [NavbarComponent, LogotypeLinkComponent],
+  exports: [NavbarComponent, FooterComponent],
+  declarations: [NavbarComponent, LogotypeLinkComponent, FooterComponent],
 })
 export class LayoutModule {}
